4124b2f0a1d6a888650865c5c4d9df58a262b44b
JonathasOliveira87 <113082597+JonathasOliveira87@users.noreply.github.com>
2023-06-12 01:43:35 -0300

    Update V1.5
    *Atualização do painel adm mp

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,4 +5,20 @@ admin.site.register(UserProfile)
 
 admin.site.register(HappyDay)
 
-admin.site.register(Message)
+
+
+
+class MessageAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        # Filtrar apenas as mensagens principais (sem respostas)
+        queryset = queryset.filter(parent_message=None)
+        return queryset
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parent_message':
+            # Filtrar as opções de parent_message para mostrar apenas o parente do tópico principal
+            kwargs['queryset'] = Message.objects.filter(parent_message=None)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+admin.site.register(Message, MessageAdmin)
